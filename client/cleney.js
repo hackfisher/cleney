c44d43d8a4693b8b9d5064b927da647c37f88d05
HackFisher <wxiaoyin@gmail.com>
2012-04-30 19:09:59 +0800

    css, text, time, tags, category etc.

--- client/cleney.js
+++ client/cleney.js
@@ -1,34 +1,52 @@
-Template.hello.greeting = function () {
-  return "Welcome to cleney.";
+var translate2date = function (list) {
+  for (var i = 0; i < list.size(); i ++) {
+    if (list[i].timestamp) {
+      list[i].timestamp = new Date(list[i].timestamp);
+    }
+  }
+  
+  return list;
 };
 
-Template.hello.events = {
-  'click input' : function () {
-    // template data, if any, is available in 'this'
-    if (typeof console !== 'undefined')
-      console.log("You pressed the button");
+
+var category_list_finder = function (x) {
+  return function () {
+    return Lists.find({status_id: x}, {sort: {timestamp: 1}});
   }
 };
-
-Template.additem.events = {
-  'click input#itemadd' : function () {
-    var value = document.getElementById("itemtext").value;
-    alert("the text is:"+ value);
-    Lists.insert({name: value}); 
-    alert($);
-    if (typeof console !== 'undefined')
-      console.log("You pressed the add button");
-  }
-}
 
 Meteor.subscribe('lists');
-//Template.lists.lists = function () {
-//  return [{name: "Test1"}, {name:"Test2"}, {name:"HackFisher"}];
-//}
-
 
 Lists = new Meteor.Collection("lists");
 
-Template.lists.lists = function () {
-  return Lists.find({}, {sort: {name: 1}});
+Meteor.startup(function () {
+$("#default_view").append(
+  Meteor.ui.render(function () {
+    return Template.item_list({list: Lists.find({}, {sort: {timestamp: 1}})});
+  })
+);
+
+for (var i = 0; i < 3; i ++) {
+  $("#category_view").append(
+    Meteor.ui.render(function () {
+      return Template.item_list({list: category_list_finder(i)});
+    })
+  );
+}
+});
+
+// add list item
+Template.add_item.events = {
+  'click input#itemadd' : function () {
+    var value = document.getElementById("itemtext").value;
+    Lists.insert({project: "default", text: value, tags: [], timestamp: (new Date()).getTime(), status_id: 0}); 
+  }
+}
+
+Template.item_list.done_class = function () {
+  return this.status_id == 1 ? 'done' : '';
+};
+
+Template.item_list.done_checkbox = function () {
+  return this.status_id == 1 ? 'checked="checked"' : '';
 };
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,14 +2,11 @@ Meteor.startup(function () {
   // code to run on server at startup
   if (Lists.find().count() === 0) {
     var data = [
-      {name: "Test1"},
-      {name: "Test2"},
-      {name: "HackFisher"},
-      {name: "ABC"}
+      {name: "Test1", status_id: 0},
     ];
 		
     for (var i = 0; i < data.length; i ++) {
-      Lists.insert({name: data[i].name});
+      Lists.insert(data[i]);
     }
   }
 });

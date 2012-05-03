528cff2583fcec315eddd569e7751d6d5e822519
HackFisher <wxiaoyin@gmail.com>
2012-05-03 16:05:57 +0800

    remove the clean data code

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -8,13 +8,5 @@ Meteor.startup(function () {
     for (var i = 0; i < data.length; i ++) {
       Lists.insert(data[i]);
     }
-  } else {
-    var lists =Lists.find();
-    lists.forEach(function (item) {
-	if (item.timestamp) {
-	} else {
-	  Lists.update({_id: item._id}, {project: "default", text: item.name, tags: [], timestamp: (new Date()).getTime(), status_id: 0});
-	}
-    });
   }
 });
